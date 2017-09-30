247178b099a1518807e912f95b2a3b235c42e65d
Anh <anhtnt@elightup.com>
2017-09-30 09:17:19 +0700

    Version 1.0.4: Add check for class exists
    It's now safe to include into themes or plugins.

--- text-limiter.php
+++ text-limiter.php
@@ -3,111 +3,115 @@
  * Plugin Name: Meta Box Text Limiter
  * Plugin URI: https://metabox.io/plugins/meta-box-text-limiter/
  * Description: Limit number of characters or words entered for text and textarea fields
- * Author: ThaoHa, Anh Tran
- * Version: 1.0.3
+ * Author: MetaBox.io
+ * Version: 1.0.4
  * Author URI: https://metabox.io
  *
- * @package Meta Box
+ * @package    Meta Box
  * @subpackage Meta Box Text Limiter
  */
 
-/**
- * Text limiter class.
- */
-class Text_Limiter {
-	/**
-	 * List of supported fields.
-	 *
-	 * @var array
-	 */
-	protected $types = array( 'text', 'textarea' );
+if ( ! class_exists( 'MB_Text_Limiter' ) ) {
 
 	/**
-	 * Constructor.
+	 * Text limiter class.
 	 */
-	public function __construct() {
-		add_action( 'rwmb_before', array( $this, 'register' ) );
+	class MB_Text_Limiter {
+		/**
+		 * List of supported fields.
+		 *
+		 * @var array
+		 */
+		protected $types = array( 'text', 'textarea' );
 
-		// Change the output of fields with limit.
-		// Pre Meta Box 4.8.2.
-		add_filter( 'rwmb_get_field', array( $this, 'get_value' ), 10, 2 );
-		add_filter( 'rwmb_the_field', array( $this, 'get_value' ), 10, 2 );
-		// Meta Box 4.8.2+.
-		add_filter( 'rwmb_get_value', array( $this, 'get_value' ), 10, 2 );
-		add_filter( 'rwmb_the_value', array( $this, 'get_value' ), 10, 2 );
+		/**
+		 * Initialize.
+		 */
+		public function init() {
+			add_action( 'rwmb_before', array( $this, 'register' ) );
 
-		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
-	}
+			// Change the output of fields with limit.
+			// Pre Meta Box 4.8.2.
+			add_filter( 'rwmb_get_field', array( $this, 'get_value' ), 10, 2 );
+			add_filter( 'rwmb_the_field', array( $this, 'get_value' ), 10, 2 );
+			// Meta Box 4.8.2+.
+			add_filter( 'rwmb_get_value', array( $this, 'get_value' ), 10, 2 );
+			add_filter( 'rwmb_the_value', array( $this, 'get_value' ), 10, 2 );
 
-	/**
-	 * Register hook to change the output of text/textarea fields.
-	 */
-	public function register() {
-		foreach ( $this->types as $type ) {
-			add_filter( "rwmb_{$type}_html", array( $this, 'show' ), 10, 2 );
-		}
-	}
-
-	/**
-	 * Change the output of text/textarea fields.
-	 *
-	 * @param string $output HTML output of the field.
-	 * @param array  $field  Field parameter.
-	 *
-	 * @return string
-	 */
-	public function show( $output, $field ) {
-		if ( ! isset( $field['limit'] ) || ! is_numeric( $field['limit'] ) || ! $field['limit'] > 0 ) {
-			return $output;
+			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
 		}
 
-		$type = isset( $field['limit_type'] ) ? $field['limit_type'] : 'character';
-		$text = 'word' === $type ? __( 'Word Count', 'text-limiter' ) : __( 'Character Count', 'text-limiter' );
+		/**
+		 * Register hook to change the output of text/textarea fields.
+		 */
+		public function register() {
+			foreach ( $this->types as $type ) {
+				add_filter( "rwmb_{$type}_html", array( $this, 'show' ), 10, 2 );
+			}
+		}
 
-		return $output . '
-			<div class="text-limiter" data-limit-type="' . esc_attr( $type ) . '">
-				<span>' . esc_html( $text ) . ':
-					<span class="counter">0</span>/<span class="maximum">' . esc_html( $field['limit'] ) . '</span>
-				</span>
-			</div>';
-	}
+		/**
+		 * Change the output of text/textarea fields.
+		 *
+		 * @param string $output HTML output of the field.
+		 * @param array  $field  Field parameter.
+		 *
+		 * @return string
+		 */
+		public function show( $output, $field ) {
+			if ( ! isset( $field['limit'] ) || ! is_numeric( $field['limit'] ) || ! $field['limit'] > 0 ) {
+				return $output;
+			}
 
-	/**
-	 * Filters the value of a field
-	 *
-	 * @see rwmb_get_field() in meta-box/inc/functions.php for explenation
-	 *
-	 * @param string $value   Field value.
-	 * @param array  $field   Field parameters.
-	 *
-	 * @return string
-	 */
-	public function get_value( $value, $field ) {
-		if ( ! in_array( $field['type'], $this->types, true ) || empty( $field['limit'] ) || ! is_numeric( $field['limit'] ) ) {
+			$type = isset( $field['limit_type'] ) ? $field['limit_type'] : 'character';
+			$text = 'word' === $type ? __( 'Word Count', 'text-limiter' ) : __( 'Character Count', 'text-limiter' );
+
+			return $output . '
+				<div class="text-limiter" data-limit-type="' . esc_attr( $type ) . '">
+					<span>' . esc_html( $text ) . ':
+						<span class="counter">0</span>/<span class="maximum">' . esc_html( $field['limit'] ) . '</span>
+					</span>
+				</div>';
+		}
+
+		/**
+		 * Filters the value of a field
+		 *
+		 * @see rwmb_get_field() in meta-box/inc/functions.php for explenation
+		 *
+		 * @param string $value Field value.
+		 * @param array  $field Field parameters.
+		 *
+		 * @return string
+		 */
+		public function get_value( $value, $field ) {
+			if ( ! in_array( $field['type'], $this->types, true ) || empty( $field['limit'] ) || ! is_numeric( $field['limit'] ) ) {
+				return $value;
+			}
+
+			$type = isset( $field['limit_type'] ) ? $field['limit_type'] : 'character';
+			if ( 'character' === $type ) {
+				return function_exists( 'mb_substr' ) ? mb_substr( $value, 0, $field['limit'] ) : substr( $value, 0, $field['limit'] );
+			}
+
+			$value = preg_split( '/\s+/', $value, - 1, PREG_SPLIT_NO_EMPTY );
+			$value = implode( ' ', array_slice( $value, 0, $field['limit'] ) );
+
 			return $value;
 		}
 
-		$type = isset( $field['limit_type'] ) ? $field['limit_type'] : 'character';
-		if ( 'character' === $type ) {
-			return function_exists( 'mb_substr' ) ? mb_substr( $value, 0, $field['limit'] ) : substr( $value, 0, $field['limit'] );
+		/**
+		 * Enqueue assets.
+		 */
+		public function admin_enqueue_scripts() {
+			// Use helper function to get correct URL to current folder, which can be used in themes/plugins.
+			list( , $url ) = RWMB_Loader::get_path( dirname( __FILE__ ) );
+
+			wp_enqueue_style( 'text-limiter', $url . 'css/text-limiter.css' );
+			wp_enqueue_script( 'text-limiter', $url . 'js/text-limiter.js', array( 'jquery' ), '', true );
 		}
-
-		$value = preg_split( '/\s+/', $value, - 1, PREG_SPLIT_NO_EMPTY );
-		$value = implode( ' ', array_slice( $value, 0, $field['limit'] ) );
-
-		return $value;
 	}
 
-	/**
-	 * Enqueue assets.
-	 */
-	public function admin_enqueue_scripts() {
-		// Use helper function to get correct URL to current folder, which can be used in themes/plugins.
-		list( , $url ) = RWMB_Loader::get_path( dirname( __FILE__ ) );
-		
-		wp_enqueue_style( 'text-limiter', $url . 'css/text-limiter.css' );
-		wp_enqueue_script( 'text-limiter', $url . 'js/text-limiter.js', array( 'jquery' ), '', true );
-	}
+	$text_limiter = new MB_Text_Limiter();
+	$text_limiter->init();
 }
-
-new Text_Limiter;
