94ca2563282875fc00adfadcf4d6013a85ef13b3
quyennv <vanquyen2703ht@gmail.com>
2024-09-12 13:57:55 +0700

    Text Limiter: Exclude HTML tags from character count

--- text-limiter.js
+++ text-limiter.js
@@ -1,9 +1,9 @@
 jQuery( function ( $ ) {
 	'use strict';
 
-	var Limiter = function( $el ) {
+	let Limiter = function ( $el ) {
 		this.$el = $el;
-	}
+	};
 
 	Limiter.prototype = {
 		// Initialize everything.
@@ -22,16 +22,16 @@ jQuery( function ( $ ) {
 
 			this.isTinymce = false;
 			if ( !this.$input.length ) {
-				let tmce = this.$el.siblings( '.tmce-active' ).contents().filter( '.wp-editor-container' ).contents().filter('textarea');
+				let tmce = this.$el.siblings( '.tmce-active' ).contents().filter( '.wp-editor-container' ).contents().filter( 'textarea' );
 
 				if ( tmce.length > 0 ) {
 					// wysiwyg in tmce mode
-					this.$tmceEditorId = tmce[0].id;
+					this.$tmceEditorId = tmce[ 0 ].id;
 					this.$input  = $( '#' + this.$tmceEditorId );
 					this.isTinymce = true;
 				} else {
 					// wysiwyg in html mode
-					this.$input = this.$el.siblings( '.html-active' ).contents().filter( '.wp-editor-container' ).contents().filter('textarea');
+					this.$input = this.$el.siblings( '.html-active' ).contents().filter( '.wp-editor-container' ).contents().filter( 'textarea' );
 				}
 				this.switchBtn = this.$el.siblings( '.wp-editor-wrap' ).contents().filter( '.wp-editor-tools' ).contents().filter( '.wp-editor-tabs' ).contents().filter( '.wp-switch-editor' );
 			}
@@ -44,14 +44,15 @@ jQuery( function ( $ ) {
 
 		// Add event listeners for 'input'.
 		addListeners: function () {
-			var that = this;
+			const that = this;
+
 			if ( !that.isTinymce ) {
 				this.$input.on( 'input', function () {
-					var value = this.value,
+					let value = this.value,
 						length = that.count( value, that.type );
 
 					if ( length > that.max && !that.isTinymce  ) {
-						value = that.subStr( value, 0, that.max, that.type );
+						value = that.cut( that, value );
 						length = that.max;
 						this.value = value;
 					}
@@ -60,15 +61,15 @@ jQuery( function ( $ ) {
 				} );
 			} else {
 				this.$input.on( 'input change', function () {
-					var value = tinyMCE.get( that.$tmceEditorId ).getContent( { format: 'text' } );
+					let value = tinyMCE.get( that.$tmceEditorId ).getContent();
 						length = that.count( value, that.type );
 
 					if ( length > that.max ) {
-						value = that.subStr( value, 0, that.max, that.type );
+						value = that.cut( that, value );
 						length = that.max;
 						this.value = value;
 
-						tinyMCE.get( that.$tmceEditorId ).setContent( value, {format : 'html'} );
+						tinyMCE.get( that.$tmceEditorId ).setContent( value, { format : 'html' } );
 						tinyMCE.activeEditor.selection.select( tinyMCE.activeEditor.getBody(), true );
 
 						// set cursor to end of value
@@ -89,13 +90,28 @@ jQuery( function ( $ ) {
 			}
 		},
 
-		// Count for text.
-		count: function ( val, type ) {
-			if ( $.trim( val ) == '' ) {
+		// Cut the content to the max length.
+		cut: ( obj, content ) => {
+			let length = obj.count( content, obj.type );
+
+			while ( length > obj.max ) {
+				content = content.substring( 0, content.length - 1 );
+				length = obj.count( content, obj.type );
+			}
+
+			return content;
+		},
+
+		// Count the length of the content, without HTML.
+		count: function ( content, type ) {
+			// Remove HTML.
+			content = content.replace( /<[^>]+>/ig, '' );
+
+			if ( $.trim( content ) == '' ) {
 				return 0;
 			}
 
-			return 'word' === type ? val.match( /\S+/g ).length : val.length;
+			return 'word' === type ? content.match( /\S+/g ).length : content.length;
 		},
 
 		// Get subString for text by word or characters.
@@ -108,7 +124,7 @@ jQuery( function ( $ ) {
 
 			return val.substr( start, lastIndexSpace );
 		}
-	}
+	};
 
 	function update() {
 		$( '.text-limiter' ).each( function () {
